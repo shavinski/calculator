9906ed65761de42e05a9788dbb8a2b2fcec8c6b0
Jakob Shavinski <shavinski.jakob@gmail.com>
2023-01-22 20:46:53 -0800

    worked on adding numbers and operators to display of calculator

--- script.js
+++ script.js
@@ -1,35 +1,103 @@
-const zero = document.querySelector('#num-0')
-const one = document.querySelector('#num-1')
+const numbers = document.querySelectorAll('.number')
+const operators = document.querySelectorAll('.operator')
+const calculatorMathDisplay = document.querySelector('.calculator-display');
+const clear = document.querySelector('.clear')
+const equals = document.querySelector('.equals');
 
-const calculatorMathDisplay = document.querySelector('.calculotr-display')
-
-one.addEventListener('click', function(e) {
-    if(click) {
-        calculatorMathDisplay.textContent = 1;
-    }
-})
-
-
+let result;
+let firstOperand = '';
+let secondOperand = '';
+let operator;
 
 
 
-function add(num1, num2) { 
-    return num1 + num2;
+// Puts numbers into top display of calculator
+for (let num of numbers) {
+    num.addEventListener('click', function(e) {
+        // calculatorMathDisplay.textContent += `${e.target.textContent}`
+        appendNums(e.target.textContent);
+
+        // Stores our first operand 
+        if(operator === undefined) {
+            firstOperand = e.target.textContent;
+        } else {
+            secondOperand = e.target.textContent;
+        };
+
+    });
 };
 
-function subtract(num1, num2) {
-    return num1 - num2;
+function appendNums(num) {
+    if (calculatorMathDisplay.textContent === '0') {
+        calculatorMathDisplay.textContent = '';
+    };
+
+        calculatorMathDisplay.textContent += num;
 };
 
-function multiply(num1, num2) {
-    return num1 * num2;
+
+// Puts operator signs into top display of the calculator 
+for (let oper of operators) {
+    oper.addEventListener('click', function(e) {
+        console.log(e);
+        appendOperators(e.target.textContent);
+        operator = e.target.textContent
+
+
+    });
 };
 
-function divide(num1, num2) {
-    return num1 / num2;
+
+function appendOperators(oper) {
+    calculatorMathDisplay.textContent += ' ';
+    calculatorMathDisplay.textContent += oper;
+    calculatorMathDisplay.textContent += ' ';
 };
 
-function operate (operator, num1, num2) {
-    return operator(num1, num2);
+
+// Clears display of calculator
+clear.addEventListener('click', function(e) {
+    clearDisplay();
+});
+
+function clearDisplay() {
+    calculatorMathDisplay.textContent = '0';
+};
+
+
+equals.addEventListener('click', function(e) {
+    operate(operator, firstOperand, secondOperand)
+});
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+function add(operand1, operand2) { 
+    return operand1 + operand2;
+};
+
+function subtract(operand1, operand2) {
+    return operand1 - operand2;
+};
+
+function multiply(operand1, operand2) {
+    return operand1 * operand2;
+};
+
+function divide(operand1, operand2) {
+    return operand1 / operand2;
+};
+
+function operate (operator, operand1, operand2) {
+    return operator(operand1, operand2);
 };
 

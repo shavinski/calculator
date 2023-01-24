f4a089296aa00a850eea82c715b9a0a3e5fec9d1
Jakob Shavinski <shavinski.jakob@gmail.com>
2023-01-23 19:40:00 -0800

    finished project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # calculator
 Calculator project odin project 
+
+Finished 1/23/23
+
+Will come back at a later to time to add the extra credit features listed in the odin project.
--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const calculatorMathDisplay = document.querySelector('.calculator-display');
 const clear = document.querySelector('.clear')
 const equals = document.querySelector('.equals');
 
-let result;
 let firstOperand = '';
 let secondOperand = '';
-let operator;
+let result = null;
+let operator = null;
+let equalsPressed = false;
+let operatorsPressed = false;
 
 
 
@@ -16,14 +18,8 @@ for (let num of numbers) {
     num.addEventListener('click', function(e) {
         // calculatorMathDisplay.textContent += `${e.target.textContent}`
         appendNums(e.target.textContent);
-
-        // Stores our first operand 
-        if(operator === undefined) {
-            firstOperand = e.target.textContent;
-        } else {
-            secondOperand = e.target.textContent;
-        };
-
+        storeFirstOperand(e.target.textContent);
+        storeSecondOperand(e.target.textContent);
     });
 };
 
@@ -31,24 +27,44 @@ function appendNums(num) {
     if (calculatorMathDisplay.textContent === '0') {
         calculatorMathDisplay.textContent = '';
     };
+    
+    calculatorMathDisplay.textContent += num;
+};
 
-        calculatorMathDisplay.textContent += num;
+function storeFirstOperand(input) {
+    if (operatorsPressed === false) {
+        firstOperand += input;
+    };
+};
+
+function storeSecondOperand(input) {
+    if (operatorsPressed === true) {
+        secondOperand += input;
+    };
 };
 
 
 // Puts operator signs into top display of the calculator 
 for (let oper of operators) {
     oper.addEventListener('click', function(e) {
-        console.log(e);
+
         appendOperators(e.target.textContent);
-        operator = e.target.textContent
+        if(operatorsPressed === true ){
+            return;
+        };
 
-
+        operator = e.target.textContent;
+        operatorsPressed = true;
     });
 };
 
 
 function appendOperators(oper) {
+
+    if(operatorsPressed === true) {
+        return;
+    };
+
     calculatorMathDisplay.textContent += ' ';
     calculatorMathDisplay.textContent += oper;
     calculatorMathDisplay.textContent += ' ';
@@ -62,23 +78,42 @@ clear.addEventListener('click', function(e) {
 
 function clearDisplay() {
     calculatorMathDisplay.textContent = '0';
+    firstOperand = '';
+    secondOperand = '';
+    operator = null;
+    result = null;
+    equalsPressed = false;
+    operatorsPressed = false;  
+
 };
 
 
+// Functionality for the equals button 
 equals.addEventListener('click', function(e) {
     operate(operator, firstOperand, secondOperand)
+    calculatorMathDisplay.textContent = result;
+
+    firstOperand = result;
+    secondOperand = '';
+    operatorsPressed = false
+    equalsPressed = true;
 });
 
 
+function operate (operator, operand1, operand2) {
+    let operandOneToNum = Number(operand1);
+    let operandTwoToNum = Number(operand2);
 
-
-
-
-
- 
-
-
-
+    if(operator === '÷') {
+        result = divide(operandOneToNum, operandTwoToNum);
+    } else if (operator === 'x') {
+        result = multiply(operandOneToNum, operandTwoToNum);
+    } else if (operator === '-') {
+        result = subtract(operandOneToNum, operandTwoToNum);
+    } else if (operator === '+') {
+        result = add(operandOneToNum, operandTwoToNum);
+    };
+};
 
 
 function add(operand1, operand2) { 
@@ -97,7 +132,4 @@ function divide(operand1, operand2) {
     return operand1 / operand2;
 };
 
-function operate (operator, operand1, operand2) {
-    return operator(operand1, operand2);
-};
 
